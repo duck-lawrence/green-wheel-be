646ed340ea294ebcef98c4617e98079e6baa9fef
phuc <hongphuc.pham.7777@gmail.com>
2025-10-02 22:18:16 +0700

    [FE] Refactor Fomikl Yub

--- frontend/src/app/register-car/page.tsx
+++ frontend/src/app/register-car/page.tsx
@@ -1,12 +1,13 @@
 'use client'
 
+import { RegisterReceiveForm } from '@/components'
 import dynamic from 'next/dynamic'
 
-const RegisterReceiveCar = dynamic(
-  () => import('@/components/shared/RegisterReceiveForm'),
-  { ssr: false }
-)
+// const RegisterReceiveCar = dynamic(
+//   () => import('@/components/shared/RegisterReceiveForm'),
+//   { ssr: false }
+// )
 
 export default function RegisterCarPage() {
-  return <RegisterReceiveCar />
+  return <RegisterReceiveForm />
 }
