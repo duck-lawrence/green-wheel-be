3a01939bd7a5b384afd9fda69fe5cd1ffa7bba53
tduc01234@gmail.com <tduc01234@gmail.com>
2025-10-02 11:27:03 +0700

    [FE] fix: eslint

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,39 +1,32 @@
-import parser from "@typescript-eslint/parser"
 import pluginNext from "@next/eslint-plugin-next"
+import parser from "@typescript-eslint/parser"
 import { defineConfig } from "eslint/config"
 
-export default defineConfig([
-    {
-        ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
-    },
-    {
-        files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-        languageOptions: {
-            parser, // optional
-            parserOptions: {
-                ecmaVersion: "latest",
-                sourceType: "module",
-                ecmaFeatures: {
-                    jsx: true
-                }
-            }
-        },
-        plugins: { "@next/next": pluginNext },
-        settings: {
-            react: {
-                version: "detect"
+export default defineConfig({
+    ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"],
+    languageOptions: {
+        parser,
+        parserOptions: {
+            ecmaVersion: "latest",
+            sourceType: "module",
+            ecmaFeatures: {
+                jsx: true
             }
         }
     },
-    {
-        rules: {
-            ...pluginNext.configs.recommended.rules,
-            ...pluginNext.configs["core-web-vitals"].rules,
-            "react/display-name": "off",
-            indent: ["error", 4],
-            "linebreak-style": "off",
-            quotes: ["error", "double"],
-            semi: ["error", "never"]
-        }
+    plugins: {
+        "@next/next": pluginNext
+    },
+    settings: {
+        react: { version: "detect" }
+    },
+    files: ["**/*.ts", "**/*.tsx"],
+    rules: {
+        ...pluginNext.configs.recommended.rules,
+        "react/display-name": "off",
+        indent: ["error", 4],
+        "linebreak-style": "off",
+        quotes: ["error", "double"],
+        semi: ["error", "never"]
     }
-])
+})
