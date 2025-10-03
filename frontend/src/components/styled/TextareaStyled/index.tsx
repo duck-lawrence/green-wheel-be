4c92a9603d6a01514505630da7638f7718c4c912
phuc <hongphuc.pham.7777@gmail.com>
2025-10-03 13:03:23 +0700

    [FE] Update TextAreaProps style in HeroUI

--- frontend/src/components/styled/TextareaStyled/index.tsx
+++ frontend/src/components/styled/TextareaStyled/index.tsx
@@ -1,9 +1,20 @@
 "use client"
-import React from "react"
-import { cn, Textarea, TextAreaProps } from "@heroui/react"
+import * as React from "react"
+import { Textarea, type TextAreaProps  , cn } from "@heroui/react"
 
-export function TextareaStyled(props: TextAreaProps) {
-    return (
-        <Textarea variant="bordered" color="primary" {...props} className={cn(props.className)} />
-    )
-}
+export const TextareaStyled = React.forwardRef<HTMLTextAreaElement, TextAreaProps>(
+  ({ className, classNames, variant = "bordered", color = "primary", ...rest }, ref) => (
+    <Textarea
+      ref={ref}                           
+      variant={variant}
+      color={color}
+      {...rest}
+      className={cn("w-full", className)}
+      classNames={{
+        inputWrapper: "min-h-[88px]",     
+        ...classNames,
+      }}
+    />
+  )
+)
+TextareaStyled.displayName = "TextareaStyled"
