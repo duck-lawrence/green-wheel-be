c68ce2f1412dc48b668d2436de463825e5f433b9
NGOGIAHUY24 <huyngse183274@fpt.edu.vn>
2025-10-06 00:30:23 +0700

    [FE] wip: en footer

--- frontend/src/components/shared/Footer/index.tsx
+++ frontend/src/components/shared/Footer/index.tsx
@@ -3,8 +3,10 @@ import { LogoStyle } from "@/components/styled"
 import { FacebookLogoIcon, InstagramLogoIcon, TiktokLogoIcon } from "@phosphor-icons/react"
 import Link from "next/link"
 import React from "react"
+import { useTranslation } from "react-i18next"
 
 export function Footer() {
+    const { t } = useTranslation()
     return (
         <footer className="bg-black text-white border-t mt-10">
             <div className="max-w-7xl mx-auto px-6 py-10 grid grid-cols-1 md:grid-cols-4 gap-8">
@@ -17,7 +19,7 @@ export function Footer() {
                         </Link>
                     </div>
 
-                    <p className="text-sm">CÔNG TY CỔ PHẦN THƯƠNG MẠI VÀ DỊCH VỤ GREEN WHEEL</p>
+                    <p className="text-sm">{t("footer.green_wheel_company_name")}</p>
                     <p className="mt-2">
                         Địa chỉ: Văn phòng 02, Tầng 08, Tòa nhà Pearl Plaza, Số 561A Điện Biên Phủ,
                         Phường 25, Quận Bình Thạnh, Thành phố Hồ Chí Minh, Việt Nam.
@@ -26,45 +28,42 @@ export function Footer() {
 
                 {/* giới thiệu*/}
                 <div className="ml-6">
-                    <h1 className="font-semibold mb-3 text-xl">Giới thiệu</h1>
+                    <h1 className="font-semibold mb-3 text-xl">{t("footer.introduction")}</h1>
                     <ul className="space-y-4 text-sm">
                         <li>
-                            <Link href="#">Về chúng tôi</Link>
+                            <Link href="#">{t("footer.about_us")}</Link>
                         </li>
                         <li>
-                            <Link href="#">Dịch vụ</Link>
-                        </li>
-                        <li>
-                            <Link href="#">Tin tức</Link>
+                            <Link href="#">{t("footer.services")}</Link>
                         </li>
                     </ul>
                 </div>
 
                 {/* Chính sách */}
                 <div className="">
-                    <h1 className="font-semibold mb-3 text-xl">Chính Sách</h1>
+                    <h1 className="font-semibold mb-3 text-xl">{t("footer.policy")}</h1>
                     <ul className="space-y-4 text-sm">
                         <li>
-                            <Link href="#">Chính sách và quy định</Link>
+                            <Link href="#">{t("footer.policy_and_regulations")}</Link>
                         </li>
                         <li>
-                            <Link href="#">Quy chế hoạt động</Link>
+                            <Link href="#">{t("footer.operating_regulations")}</Link>
                         </li>
                         <li>
-                            <Link href="#">Bảo mật thông tin</Link>
+                            <Link href="#">{t("footer.information_security")}</Link>
                         </li>
                         <li>
-                            <Link href="#">Giải quyết tranh chấp</Link>
+                            <Link href="#">{t("footer.dispute_resolution")}</Link>
                         </li>
                     </ul>
                 </div>
 
                 {/* Đối tác */}
                 <div>
-                    <h1 className="font-semibold mb-3 text-xl">Liên hệ</h1>
+                    <h1 className="font-semibold mb-3 text-xl">{t("footer.contact")}</h1>
                     <ul className="space-y-4 text-sm">
                         <li>
-                            <Link href="#">Hotline: 0797 123 432</Link>
+                            <Link href="#">{t("footer.hotline")}: 0797 123 432</Link>
                         </li>
                         <li>
                             <Link href="#">Email: greenwhell.work@gmail.com</Link>
@@ -87,7 +86,7 @@ export function Footer() {
 
             {/* Company info */}
             <div className="border-t py-6 px-6 text-sm text-white max-w-7xl mx-auto">
-                <p>© 2025 Green Wheel corp. All rights reserved.</p>
+                <p>{t("footer.copyright_notice")}</p>
             </div>
         </footer>
     )
