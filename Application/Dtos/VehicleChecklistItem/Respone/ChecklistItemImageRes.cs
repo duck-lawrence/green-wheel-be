86ff71aec7e84e97ff7eda5eac76f0e37e6e4d60
WuagHy <nguyenquanghuy14022005@gmail.com>
2025-10-12 00:02:08 +0700

    fix: check list item pict

---
+++ Application/Dtos/VehicleChecklistItem/Respone/ChecklistItemImageRes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dtos.VehicleChecklistItem.Respone
+{
+    public class ChecklistItemImageRes
+    {
+        public Guid ItemId { get; set; }
+        public string ImageUrl { get; set; } = null!;
+        public string Message { get; set; } = null!;
+    }
+}
