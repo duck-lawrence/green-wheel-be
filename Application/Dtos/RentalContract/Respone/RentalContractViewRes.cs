0fddd2cf0acad3308f431e8fdd4d2de1c10d40db
tduc01234@gmail.com <tduc01234@gmail.com>
2025-10-12 21:15:14 +0700

    fix: contract view res

--- Application/Dtos/RentalContract/Respone/RentalContractViewRes.cs
+++ Application/Dtos/RentalContract/Respone/RentalContractViewRes.cs
@@ -1,6 +1,6 @@
-﻿using Application.Dtos.Deposit.Respone;
-using Application.Dtos.Invoice.Response;
+﻿using Application.Dtos.Invoice.Response;
 using Application.Dtos.Station.Respone;
+using Application.Dtos.User.Respone;
 using Application.Dtos.Vehicle.Respone;
 using System;
 using System.Collections.Generic;
@@ -13,21 +13,24 @@ namespace Application.Dtos.RentalContract.Respone
     public class RentalContractViewRes
     {
         public Guid Id { get; set; }
-        public string? Description { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public DateTimeOffset UpdatedAt { get; set; }
+        public string Description { get; set; } = null!;
         public string? Notes { get; set; }
         public DateTimeOffset StartDate { get; set; }
-        public DateTimeOffset? ActualStartDate { get; set; } = null;
+        public DateTimeOffset? ActualStartDate { get; set; }
         public DateTimeOffset EndDate { get; set; }
-        public DateTimeOffset? ActualEndDate { get; set; } = null;
+        public DateTimeOffset? ActualEndDate { get; set; }
+
+        public bool IsSignedByStaff { get; set; }
+        public bool IsSignedByCustomer { get; set; }
         public int Status { get; set; }
+
         public VehicleViewRes Vehicle { get; set; }
-        public Guid CustomerId { get; set; }
+        public UserProfileViewRes Customer { get; set; }
         public StationViewRes Station { get; set; }
-        public Guid? HandoverStaffId { get; set; } = null;
-        public Guid? ReturnStaffId { get; set; } = null;
+        public Guid? HandoverStaffId { get; set; }
+        public Guid? ReturnStaffId { get; set; }
         public IEnumerable<InvoiceViewRes> Invoices { get; set; }
-        
-
-
     }
 }
