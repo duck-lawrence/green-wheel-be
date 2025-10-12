2e391267c0696d52b1d937f023c317c72d9c9c87
Nguyễn Quang Huy <nguyenquanghuy14022005@gmail.com>
2025-10-12 21:48:59 +0700

    feat: patch licence and citizen id
    * fix:ticket
    
    * uia
    
    * fix:ticket
    
    * fix:ticket
    
    * fix: staff field name
    
    * feat:ticket
    
    * fix: check list item pict
    
    * fix:license
    
    * feat:multi find
    
    * feat:multi-find
    
    * feat/update-license
    
    ---------
    
    Co-authored-by: tduc01234@gmail.com <tduc01234@gmail.com>

---
+++ Application/Dtos/CitizenIdentity/Request/UpdateCitizenIdentityReq.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dtos.CitizenIdentity.Request
+{
+    public class UpdateCitizenIdentityReq
+    {
+        public string? Number { get; set; }
+        public string? FullName { get; set; }
+        public string? Nationality { get; set; }
+        public int? Sex { get; set; }
+        public DateTimeOffset? DateOfBirth { get; set; }
+        public DateTimeOffset? ExpiresAt { get; set; }
+    }
+}
---
+++ Application/Dtos/DriverLicense/Request/UpdateDriverLicenseReq.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dtos.DriverLicense.Request
+{
+    public class UpdateDriverLicenseReq
+    {
+        public string? Number { get; set; }
+        public int? Class { get; set; }
+        public string? FullName { get; set; }
+        public string? Nationality { get; set; }
+        public int? Sex { get; set; }
+        public DateTimeOffset? DateOfBirth { get; set; }
+        public DateTimeOffset? ExpiresAt { get; set; }
+    }
+}
