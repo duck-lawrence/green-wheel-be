37aec27908cb7c59ff94c07523f2a6f27514b6f1
WuagHy <nguyenquanghuy14022005@gmail.com>
2025-10-17 18:23:07 +0700

    fix: model and dispatch

--- Application/Helpers/DispatchValidationHelper.cs
+++ Application/Helpers/DispatchValidationHelper.cs
@@ -1,4 +1,5 @@
 ﻿using Application.AppExceptions;
+using Application.Constants;
 using Application.Repositories;
 using System;
 using System.Collections.Generic;
@@ -17,21 +18,35 @@ namespace Application.Helpers
 
             var countValid = await staffRepository.CountStaffsInStationAsync(staffIds, fromStationId);
             if (countValid != staffIds.Length)
-                throw new BadRequestException("To many staff not belong to station");
+                throw new BadRequestException(Message.DispatchMessage.StaffNotInFromStation);
         }
 
         public static async Task ValidateVehiclesInStationAsync(IVehicleRepository vehicleRepository, Guid[]? vehicleId, Guid stationId)
         {
-            if (vehicleId.Length == 0 || vehicleId == null) return;
+            if (vehicleId == null || vehicleId.Length == 0) return;
 
             var countValidate = await vehicleRepository.CountVehiclesInStationAsync(vehicleId, stationId);
             if (countValidate != vehicleId.Length)
-                throw new BadRequestException("some vehicles not in station");
+                throw new BadRequestException(Message.DispatchMessage.VehicleNotInFromStation);
         }
 
         public static void EnsureDifferentStations(Guid fromStationId, Guid toStationId)
         {
-            if (fromStationId == toStationId) throw new ForbidenException("two station same ID");
+            if (fromStationId == toStationId) throw new ForbidenException(Message.DispatchMessage.ToStationMustDifferent);
+        }
+
+        public static void EnsureCanUpdate(
+            Guid currentStationId,
+            Guid expectedStationId,
+            DispatchRequestStatus currentStatus,
+            DispatchRequestStatus requiredStatus,
+            string forbiddenMessage,
+            string invalidStatusMessage)
+        {
+            if (currentStationId != expectedStationId)
+                throw new ForbidenException(forbiddenMessage);
+            if (currentStatus != requiredStatus)
+                throw new BadRequestException(invalidStatusMessage);
         }
     }
 }
