9cb2f508906f877abc9abd4cd4ec54531123f53b
wuanghy <nguyenquanghuy14022005@gmail.com>
2025-10-23 13:26:18 +0700

    feat: implement token verify swagger

--- API/Program.cs
+++ API/Program.cs
@@ -40,7 +40,46 @@ namespace API
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+
+            //builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Title = "GreenWheel API",
+                    Version = "v1",
+                    Description = "Tài liệu Swagger cho hệ thống thuê xe GreenWheel"
+                });
+
+                c.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+                {
+                    Description = @"JWT Authorization header sử dụng scheme Bearer. 
+                        Nhập vào chuỗi như sau: 'Bearer <token>'.",
+                    Name = "Authorization",
+                    In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+                    Type = Microsoft.OpenApi.Models.SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    BearerFormat = "JWT"
+                });
+
+                c.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+                {
+                    {
+                        new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+                        {
+                            Reference = new Microsoft.OpenApi.Models.OpenApiReference
+                            {
+                                Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = Microsoft.OpenApi.Models.ParameterLocation.Header
+                        },
+                        new List<string>()
+                    }
+                });
+            });
 
             //Cors frontEnd
             builder.Services.AddCors(options =>
